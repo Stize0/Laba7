e927ff1ce567923dea4c2bfe2e2433ffb32273d9
Stize <stize9@gmail.com>
2024-12-20 00:08:02 +0700

    Commit послеХ3 начала

--- app/src/main/java/api/FlickrApi.kt
+++ app/src/main/java/api/FlickrApi.kt
@@ -1,8 +1,10 @@
 package api
+import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Url
 interface FlickrApi {
-        @GET(
+    @GET(
         "services/rest/?method=flickr.interestingness.getList" +
                 "&api_key=8201599d7f5b66ca70768630f67427e1" +
                 "&format=json" +
@@ -10,4 +12,6 @@ interface FlickrApi {
                 "&extras=url_s"
     )
     fun fetchPhotos(): Call<FlickrResponse>
+    @GET
+    fun fetchUrlBytes(@Url url: String) : Call<ResponseBody>
 }
--- app/src/main/java/com/bignerdranch/android/photogallery/FlickrFetchr.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery/FlickrFetchr.kt
@@ -8,20 +8,26 @@ import api.PhotoResponse
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import androidx.annotation.WorkerThread
 import retrofit2.Retrofit
+import okhttp3.ResponseBody
 import retrofit2.converter.scalars.ScalarsConverterFactory
 import retrofit2.converter.gson.GsonConverterFactory
+
+
 private const val TAG = "FlickrFetchr"
 class FlickrFetchr {
     private val flickrApi: FlickrApi
     init {
         val retrofit: Retrofit = Retrofit.Builder()
-                    .baseUrl("https://api.flickr.com/")
+            .baseUrl("https://api.flickr.com/")
             .addConverterFactory(GsonConverterFactory.create())
             .build()
         flickrApi = retrofit.create(FlickrApi::class.java)
     }
-        fun fetchPhotos(): LiveData<List<GalleryItem>> {
+    fun fetchPhotos(): LiveData<List<GalleryItem>> {
         val responseLiveData : MutableLiveData<List<GalleryItem>> = MutableLiveData()
         val flickrRequest: Call<FlickrResponse> = flickrApi.fetchPhotos()
         flickrRequest.enqueue(object : Callback<FlickrResponse> {
@@ -34,7 +40,7 @@ class FlickrFetchr {
                 response: Response<FlickrResponse>
             ) {
                 Log.d(TAG, "Response received")
-                                val flickrResponse : FlickrResponse? = response.body()
+                val flickrResponse : FlickrResponse? = response.body()
                 val photoResponse : PhotoResponse? = flickrResponse?.photos
                 var galleryItems : List<GalleryItem> = photoResponse?.galleryItems
                     ?: mutableListOf()
@@ -46,3 +52,11 @@ class FlickrFetchr {
         })
         return responseLiveData
     }
+    @WorkerThread
+    fun fetchPhoto(url: String): Bitmap? {
+        val response: Response<ResponseBody> = flickrApi.fetchUrlBytes(url).execute()
+        val bitmap = response.body()?.byteStream()?.use(BitmapFactory::decodeStream)
+        Log.i(TAG, "Decoded bitmap=$bitmap from Response=$response")
+        return bitmap
+    }
+}
